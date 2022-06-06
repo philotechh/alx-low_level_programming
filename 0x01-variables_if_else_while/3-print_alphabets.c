2424e2d7e6d10b4c56a7b914363ec81d192157e9
abimbolasherifah <abimbolasherifah205@gmail.com>
2022-06-06 01:45:32 -0700

    m

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,8 +11,10 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
+
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
+
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 

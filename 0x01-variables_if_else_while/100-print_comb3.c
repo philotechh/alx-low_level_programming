e5c32576b7341764703ec899fcefaa3f34316e8a
abimbolasherifah <abimbolasherifah205@gmail.com>
2022-06-06 17:30:20 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,17 +12,20 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		putchar((digit1 % 10) + '0');
-		putchar((digit2 % 10) + '0');
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-		if (digit1 == 8 && digit2 == 9)
-			continue;
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
+		}
 	}
-}
-putchar('\n');
 
-return (0);
+	putchar('\n');
+
+	return (0);
 }

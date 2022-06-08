3db57c5511c99d6a6ad6b4844f5602073486b051
Alimi Kehinde Sherifah <101565387+abimbolasherifah@users.noreply.github.com>
2022-06-08 01:55:34 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
-#include "main.h" 
- /** 
-  * print_alphabet - Prints the alphabet in lowercase 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
-  
- void print_alphabet(void) 
- { 
-         char alpha; 
-  
-         for (alpha = 'a'; alpha <= 'z'; alpha++) 
-         { 
-                 _putchar (alpha); 
-         } 
-         _putchar('\n'); 
- }
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

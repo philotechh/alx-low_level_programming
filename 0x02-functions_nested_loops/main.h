9ae5a7c905fce52eb62d9acbbaf6afd7b142029d
Alimi Kehinde Sherifah <101565387+abimbolasherifah@users.noreply.github.com>
2022-06-08 02:20:38 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef _main_h_
+#define _main_h_
 
-int_putchar(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif

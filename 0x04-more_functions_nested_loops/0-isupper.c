c104c2e84576177dc77c5e419962b0e0cc9d4338
Alimi Kehinde Sherifah <101565387+abimbolasherifah@users.noreply.github.com>
2022-06-10 00:14:59 +0100

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check for uppercharacter.
+ *
+ * Return: Always 0. 
+ */ 
+  int main(void)
+{
+        char ch = 'A';
+
+      if (isupper(ch)) 
+
+        printf("%d \n");
+
+        else
+
+                printf("%d\n");
+        return (0);
+}

ea5f9b52585486ca099498c64b9dfa2fc93ba9f2
Alimi Kehinde Sherifah <101565387+abimbolasherifah@users.noreply.github.com>
2022-06-10 03:55:55 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,46 +1,17 @@
 #include "main.h"
-<<<<<<< HEAD
+
 /**
- * main - print number 0-9
- *
- * Return : Always 0 (success)
+ * print_numbers - a function that prints a number from 0 to 9
+ * followed by a new line
+ * Return: Nothing
  */
-
-void print_numbers(void);
-=======
-
-
- /**
- *
- * main - Prints _putchar as a message.
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
->>>>>>> 10ef0c367368044f6a441cb653706b85be855a71
-
+void print_numbers(void)
 {
+	int a;
 
-    int str[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
-
-    int count, sz;
-
-
-    sz = sizeof(str) / sizeof(int);
-
-    for (count = 0; count < sz; count++)
-
-    {
-
-        _putchar(str[count]);
-
-    }
-
-    _putchar('\n');
-
-    return (0);
-
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
 }
-
-

bea5c867b74e04eeaf1e5787df7bb2715014dee5
Alimi Kehinde Sherifah <101565387+abimbolasherifah@users.noreply.github.com>
2022-06-10 02:49:03 +0100

    Create 3-print_remaining_days.c

---
+++ 0x04-more_functions_nested_loops/3-print_remaining_days.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+/**
+
+ * main - Prints _putchar as a message.
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+    int str[] = {48,49, 50, 51, 52, 53, 54, 55, 56, 57};
+
+    int count, sz;
+
+
+    sz = sizeof(str) / sizeof(int);
+
+    for (count = 0; count < sz; count++)
+
+    {
+
+        _putchar(str[count]);
+
+    }
+
+    _putchar('\n');
+
+    return (0);
+
+}
+
+

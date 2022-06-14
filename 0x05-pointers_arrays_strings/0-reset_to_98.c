2ed916c179d51a68ba26db6d3606d0f1d23085c1
abimbolasherifah <abimbolasherifah205@gmail.com>
2022-06-13 17:05:57 -0700

    m

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main- function that takes a pointer to an int
+ *
+ * Return:(void)
+ *
+ */
+
+void reset_to_98(int *n);
+{
+	int p;
+
+	int n;
+
+	p = 98
+		;
+	n = &p;
+
+	return (0);
+}

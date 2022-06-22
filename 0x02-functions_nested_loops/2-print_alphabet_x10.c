35a34ea88b03ba2021d91ca35d149d1de6dce906
abimbolasherifah <abimbolasherifah205@gmail.com>
2022-06-22 02:22:22 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for (i = 48 ; i >= 57 ; i++)
+	for (i = 48 ; i <= 57 ; i++)
 	{
-		for (i = 97 ; i >= 122 ; i++)
+		for (i = 97 ; i <= 122 ; i++)
 			_putchar(i);
 	}
 	_putchar('\n');

433e1176a20c43767ef9c8a3a576da5cb9179b9b
abimbolasherifah <abimbolasherifah205@gmail.com>
2022-06-24 14:47:17 -0700

    m

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Shows 1 if the input is a
+ * lowercase character. Another cases, shows
+ * 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character, 0 for others
+ */
+int _islower(int c)
+{
+	if ((c >= 'a') && (c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

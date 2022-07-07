038e8f1dc9c759d2e396d8a1a2703c2e5961f238
Alimi Kehinde Sherifah <101565387+abimbolasherifah@users.noreply.github.com>
2022-07-08 00:53:31 +0100

    Create 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ *
+ * Return: no return.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
